956bf6864270d069cf1cd600ff1ffd2c5e8fd820
AlefAdonis <alefadonis10@gmail.com>
2020-09-22 19:52:13 -0300

    Adicionando a license.md e novas escolhas e retirada da segunda personagem

---
+++ main_game/creditos.py
@@ -0,0 +1,6 @@
+def credits():
+	
+    print('\nObrigado por jogar !\n')
+    print('-' * 20)
+    print('CRÉDITOS FINAIS \nDiretor Geral - Álef Ádonis\nProgramador - Álef Ádonis\nRoteirista - Álef Ádonis\nEditor Chefe - Álef Ádonis\n\nUM PEQUENO GESTO PARA DEMOSTRAR MEU AMOR')
+    print('-' *20)
